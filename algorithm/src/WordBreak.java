b11b608031c33d9356875944bc0cf683ce818a4c
Administrator <hankin_06@outlook.com>
2020-06-25 15:28:46 +0800

    for update

---
+++ algorithm/src/WordBreak.java
@@ -0,0 +1,26 @@
+/**
+ * @author hankin
+ * @date 2020/6/25 13:51
+ */
+
+/**
+ * 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，
+ * 判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+ * 拆分时可以重复使用字典中的单词。
+ * 你可以假设字典中没有重复的单词。
+ * <p>
+ * 输入: s = "leetcode", wordDict = ["leet", "code"]
+ * 输出: true
+ * 解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+ * <p>
+ * 题源：139
+ */
+public class WordBreak {
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+}
