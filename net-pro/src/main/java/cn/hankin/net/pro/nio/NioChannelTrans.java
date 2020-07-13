6711ed118581e50b7c101182ed762bb452082355
huajin <hankin.hua@analyticservice.net>
2020-07-13 15:40:15 +0800

    nio FileChannel

---
+++ net-pro/src/main/java/cn/hankin/net/pro/nio/NioChannelTrans.java
@@ -0,0 +1,27 @@
+package cn.hankin.net.pro.nio;
+
+import java.io.*;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author hankin
+ * @date 2020/7/13 15:23
+ */
+public class NioChannelTrans {
+    public static void main(String[] args) throws IOException {
+        File file = new File("out.txt");
+        FileOutputStream out =new FileOutputStream("out2.txt");
+        FileInputStream in =new FileInputStream(file);
+
+        FileChannel outChannel = out.getChannel();
+        FileChannel inChannel = in.getChannel();
+
+//        outChannel.transferFrom(inChannel,0,inChannel.size());
+
+        inChannel.transferTo(0,inChannel.size(),outChannel);
+        outChannel.close();
+        inChannel.close();
+        in.close();
+        out.close();
+    }
+}
