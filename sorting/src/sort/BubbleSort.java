469053aa961140c8011d376d809e17e1c0f55c28
Administrator <hankin_06@outlook.com>
2020-09-26 16:42:28 +0800

    modify some codes

--- sorting/src/sort/BubbleSort.java
+++ sorting/src/sort/BubbleSort.java
@@ -17,8 +17,8 @@ public class BubbleSort {
     }
 
     private static void bubble(int[] arr, int n) {
-        boolean flag =false;
         for (int i = 0; i < n - 1; i++) {
+            boolean flag =false;
             for (int j = 0; j < n - 1 - i; j++) {
                 if (arr[j + 1] < arr[j]) {
                     int temp = arr[j + 1];
